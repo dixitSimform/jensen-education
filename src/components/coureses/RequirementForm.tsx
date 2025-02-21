6c5ab8c994bfd25387d25208baebdaf941e12a38
Mitul <mitul.p@simformsolutions.com>
2025-02-21 16:32:56 +0530

    Fix: Unused var remove

--- src/components/coureses/RequirementForm.tsx
+++ src/components/coureses/RequirementForm.tsx
@@ -1,6 +1,6 @@
 import { Form } from "react-bootstrap";
 import { FormDialog } from "../common/FormDialog";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useForm, Controller } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
